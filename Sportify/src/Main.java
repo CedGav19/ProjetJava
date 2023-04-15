c61c6042286999f686b96a85d1fdf6bbd51cbcc2
RomainDemonty <romain.demonty@student.hepl.be>
2023-04-15 12:22:33 +0200

    First commit

--- Sportify/src/Main.java
+++ Sportify/src/Main.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 
 public class Main {
     private JPanel panel1;
-    private JTextField textField1;
     private JPasswordField passwordField1;
+    private JButton seConnecterButton;
+    private JButton pasEncoreDeCompteButton;
+    private JTextField textField1;
 }
