79990d7e5e2724173bd96d58ceab1de64fde7f1d
RomainDemonty <romain.demonty@student.hepl.be>
2023-04-20 12:15:38 +0200

    Inteface graphique de home et connexion

--- Sportify/src/Home.java
+++ Sportify/src/Home.java
@@ -2,6 +2,16 @@ import javax.swing.*;
 
 public class Home {
     private JScrollBar scrollBar1;
-    private JScrollBar scrollBar2;
     private JScrollBar scrollBar3;
+    private JList list1;
+    private JCheckBox perdre5KgCheckBox;
+    private JCheckBox courirUneHeureCheckBox;
+    private JScrollBar scrollBar2;
+    private JButton faitButton;
+    private JList list2;
+    private JProgressBar progressBar1;
+    private JProgressBar progressBar2;
+    private JScrollBar scrollBar4;
+    private JCheckBox a100AbdosCheckBox;
+    private JButton pasFaitButton;
 }
