b71e7de294e2c3c2ec93001df6045a9164a4fa91
RomainDemonty <romain.demonty@student.hepl.be>
2023-06-04 12:42:52 +0200

    Début home

--- Sportify/src/Swing/Home.java
+++ Sportify/src/Swing/Home.java
@@ -1,18 +1,37 @@
 package Swing;
 
-import Swing.Menu.Menu;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Home {
-    private JCheckBox perdre5KgCheckBox;
-    private JCheckBox courirUneHeureCheckBox;
+public class Home extends JFrame implements ActionListener {
     private JButton faitButton;
     private JProgressBar progressBar1;
     private JProgressBar progressBar2;
     private JButton pasFaitButton;
-    private JList list1;
+    private JButton ajouterSéanceButton;
+    private JScrollPane ScrollPanelObjAreal;
+    private JScrollPane ScrolpanelOjectifRealise;
+    private JPanel contentPane;
 
+    public static void main(String[] args) {
+        Home P = new Home();
+    }
+
+    Home(){
+        setContentPane(contentPane);
+        setVisible(true );
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Home");
+        setSize(800,600);
+        //Swing.Menu.Menu menubar= new Menu();
+        //setJMenuBar(menubar);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 
    /* public void main()
     {
